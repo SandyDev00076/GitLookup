44b191f7b39a108623dba451b5a6020213e56954
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-07 00:44:21 +0530

    add the debouncing logic

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -2,14 +2,26 @@ import { useQuery } from "react-query";
 import { fetchUsers } from "api/usersApi";
 import { SearchResult } from "types/SearchResult";
 import { ApiError } from "types/Error";
+import { useDebounce } from "use-debounce";
+
+/**
+ * Duration for debouncing in ms
+ */
+const DEBOUNCE_DURATION = 600;
 
 export const useUsers = (searchQuery: string, perPage: number = 20) => {
-  const result = useQuery<SearchResult, ApiError>(["users", searchQuery], () =>
-    fetchUsers(searchQuery, perPage)
+  const [debouncedSearchQuery, controlFns] = useDebounce(
+    searchQuery,
+    DEBOUNCE_DURATION
+  );
+  const result = useQuery<SearchResult, ApiError>(
+    ["users", debouncedSearchQuery],
+    () => fetchUsers(debouncedSearchQuery, perPage)
   );
   const userList = result.data !== undefined ? result.data["items"] : [];
   return {
     ...result,
+    isLoading: result.isLoading || controlFns.isPending(),
     data: userList,
   };
 };
