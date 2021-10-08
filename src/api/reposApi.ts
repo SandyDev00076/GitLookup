33a27d4b957f577e1a952fab9fc9ada0a62cea23
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-08 23:26:27 +0530

    add a way to parse link headers

--- src/api/reposApi.ts
+++ src/api/reposApi.ts
@@ -1,3 +1,5 @@
+import parseLink from "parse-link-header";
+
 /**
  * Returns a list of public repos
  * @param url URL through which repos will be fetched
@@ -5,5 +7,7 @@
  */
 export async function fetchRepos(url: string) {
   const res = await fetch(url);
+  const linkHeader = res.headers.get("Link");
+  if (linkHeader) console.log("Next link - ", parseLink(linkHeader));
   return await res.json();
 }
